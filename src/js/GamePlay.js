3724a37f362e829dd24beac79f77ba9c2f4d2abe
a-naraikin <anna.naraikin@yandex.ru>
2021-11-12 16:02:19 +0300

    remove create board, remove create and move sprite, add buttons

--- src/js/GamePlay.js
+++ src/js/GamePlay.js
@@ -1,32 +1,81 @@
-/* eslint-disable prefer-const */
+/* eslint-disable eqeqeq */
 /* eslint-disable no-plusplus */
-/* eslint-disable class-methods-use-this */
+import Board from './Board';
+import Sprite from './Sprite';
+
 export default class GamePlay {
   constructor() {
     this.size = 4; // кол-во ячеек в массиве
-    this.boardEl = document.getElementById('board');
-    this.positionPersonage = -1;
+    this.modalEl = document.getElementById('modal');
+    this.countDead = null;
+    this.countLost = null;
   }
 
   startGame() {
-    this.board = this.initiationBoard(this.size); // инициализация начального массива
-    this.renderBoard(this.board);
+    const board = new Board();
+    board.initiationBoard(this.size);
+
+    const sprite = new Sprite();
+
+    this.onCellClick();
+    this.onButtonClick();
 
     setInterval(() => {
-      this.randomPositionPersonage();
+      sprite.randomPositionSprite(this.size);
     }, 1000);
   }
 
-  randomPositionPersonage() {
-    const divs = document.querySelectorAll('.field');
+  onCellClick() {
+    const fields = document.querySelectorAll('.field');
 
-    const randomInt = Math.floor(Math.random() * (this.size ** 2));
-    if (this.positionPersonage >= 0) {
-      divs[this.positionPersonage].classList.remove('free');
-      this.positionPersonage = -1;
+    this.countDead = document.getElementById('dead');
+    this.countLost = document.getElementById('lost');
+
+    for (let i = 0; i < fields.length; i++) {
+      fields[i].addEventListener('click', () => {
+        if (fields[i].classList.contains('sprite')) {
+          fields[i].classList.add('cursor_hammer');
+          setTimeout(() => fields[i].classList.remove('cursor_hammer'), 1000);
+          fields[i].classList.remove('sprite');
+          this.countDead.textContent = +this.countDead.textContent + 1;
+        } else {
+          this.countLost.textContent = +this.countLost.textContent + 1;
+        }
+
+        if (this.countDead.textContent == 10) {
+          this.showWinner('🍾 Победа! 🍾');
+          this.reset();
+        }
+
+        if (this.countLost.textContent == 5) {
+          this.showWinner('Вы проиграли!');
+          this.reset();
+        }
+      });
     }
+  }
 
-    divs[randomInt].classList.add('free');
-    this.positionPersonage = randomInt;
+  onButtonClick() {
+    const resetButtons = document.querySelectorAll('.reset');
+
+    for (const btn of resetButtons) {
+      btn.addEventListener('click', () => {
+        if (!this.modalEl.classList.contains('hidden')) {
+          this.modalEl.classList.add('hidden');
+        }
+        this.reset();
+      });
+    }
+  }
+
+  reset() {
+    this.countDead.textContent = 0;
+    this.countLost.textContent = 0;
+  }
+
+  showWinner(status) {
+    const header = this.modalEl.getElementsByTagName('h2')[0];
+    header.textContent = status;
+    this.modalEl.classList.remove('hidden');
   }
 }
